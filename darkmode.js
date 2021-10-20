bccda2b0391d2eb996e14b0b29b3c22a49284d40
abderahman403 <72399201+abderahman403@users.noreply.github.com>
2021-10-20 20:08:53 +0100

    Create darkmode.js

---
+++ darkmode.js
@@ -0,0 +1,6 @@
+let darkMode = () => {
+    document.body.classList.toggle('dark');
+    if(document.body.classList.contains('dark')) localStorage.setItem('darkmode','on');
+    else localStorage.removeItem('darkmode');   
+}
+if(localStorage.getItem('darkmode') == 'on') document.body.classList.add('dark');
